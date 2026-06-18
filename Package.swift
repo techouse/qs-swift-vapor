79f875e6a071eedbc2e3512eb7143756ac380441
Klemen Tusar <techouse@gmail.com>
2026-06-18 08:47:24 +0100

    chore: format Package.swift for improved readability

--- Package.swift
+++ Package.swift
@@ -3,38 +3,38 @@
 import PackageDescription
 
 let package = Package(
-    name: "QsSwiftVapor",
-    platforms: [
-        .macOS(.v12),
-    ],
-    products: [
-        .library(
-            name: "QsSwiftVapor",
-            targets: ["QsSwiftVapor"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/techouse/qs-swift.git", from: "1.4.0"),
-        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.6"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.121.2"),
-    ],
-    targets: [
-        .target(
-            name: "QsSwiftVapor",
-            dependencies: [
-                .product(name: "QsSwift", package: "qs-swift"),
-                .product(name: "Vapor", package: "vapor"),
-            ],
-            path: "Sources/QsSwiftVapor"
-        ),
-        .testTarget(
-            name: "QsSwiftVaporTests",
-            dependencies: [
-                "QsSwiftVapor",
-                .product(name: "QsSwift", package: "qs-swift"),
-                .product(name: "XCTVapor", package: "vapor"),
-            ],
-            path: "Tests/QsSwiftVaporTests"
-        ),
-    ]
+  name: "QsSwiftVapor",
+  platforms: [
+    .macOS(.v12)
+  ],
+  products: [
+    .library(
+      name: "QsSwiftVapor",
+      targets: ["QsSwiftVapor"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/techouse/qs-swift.git", from: "1.4.0"),
+    .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.6"),
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.121.2")
+  ],
+  targets: [
+    .target(
+      name: "QsSwiftVapor",
+      dependencies: [
+        .product(name: "QsSwift", package: "qs-swift"),
+        .product(name: "Vapor", package: "vapor")
+      ],
+      path: "Sources/QsSwiftVapor"
+    ),
+    .testTarget(
+      name: "QsSwiftVaporTests",
+      dependencies: [
+        "QsSwiftVapor",
+        .product(name: "QsSwift", package: "qs-swift"),
+        .product(name: "XCTVapor", package: "vapor")
+      ],
+      path: "Tests/QsSwiftVaporTests"
+    )
+  ]
 )
